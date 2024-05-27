51a8621a1ac50d68143e675d69c9abecdec16818
Ppajingae <wkmkyj991112@gmail.com>
2024-05-27 20:16:48 +0900

    #3 패키지 구조 재설정 진행

---
+++ bin/main/sparta/nbcamp/gamenomeprojectserver/GamenomeProjectServerApplication.kt
@@ -0,0 +1,11 @@
+package sparta.nbcamp.gamenomeprojectserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GamenomeProjectServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<GamenomeProjectServerApplication>(*args)
+}
