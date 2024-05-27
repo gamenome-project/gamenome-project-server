fbdf269029588641eac702e12e761a9db7c15299
임성우 <seongwoorang@naver.com>
2024-05-27 19:13:40 +0900

    Feat: #1 ReviewController 추가

---
+++ src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/review/controller/v1/ReviewController.kt
@@ -0,0 +1,11 @@
+package sparta.nbcamp.gamenomeprojectserver.domain.review.controller.v1
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import sparta.nbcamp.gamenomeprojectserver.domain.ApiV1MappingConfig
+
+@RestController
+@RequestMapping("/reviews")
+class ReviewController : ApiV1MappingConfig() {
+
+}
