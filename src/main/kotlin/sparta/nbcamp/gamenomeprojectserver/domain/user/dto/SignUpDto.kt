5fe29728e62726755dec0d3aab5c55756e24782f
Jaewon Noh <opqw987@gmail.com>
2024-05-28 15:38:36 +0900

    Feat: User 기본 비즈니스 로직 작성

--- src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/user/dto/SignUpDto.kt
+++ src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/user/dto/SignUpDto.kt
@@ -3,5 +3,6 @@ package sparta.nbcamp.gamenomeprojectserver.domain.user.dto
 data class SignUpDto (
     val email: String,
     val password: String,
-    val nickname: String
+    val nickname: String,
+    val aboutSummary: String?,
 )
--- src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/user/repository/UserRepository.kt
@@ -3,4 +3,6 @@ package sparta.nbcamp.gamenomeprojectserver.domain.user.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import sparta.nbcamp.gamenomeprojectserver.domain.user.model.User
 
-interface UserRepository : JpaRepository<User, Long>
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByEmail(email: String): User?
+}
