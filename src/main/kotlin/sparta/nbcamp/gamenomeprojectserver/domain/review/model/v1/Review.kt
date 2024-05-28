bdd86d1b4d6627d721a7765d2f9a3aa219e10440
임성우 <seongwoorang@naver.com>
2024-05-28 12:09:58 +0900

    Feat: #1 CreationTimestamp, UpdateTimestamp 어노테이션 추가

--- src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/review/model/v1/Review.kt
+++ src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/review/model/v1/Review.kt
@@ -1,6 +1,8 @@
 package sparta.nbcamp.gamenomeprojectserver.domain.review.model.v1
 
 import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDateTime
 
 @Entity
@@ -18,14 +20,16 @@ class Review(
     @Column(name = "is_deleted", nullable = false)
     var isDeleted: Boolean = false,
 
+    @CreationTimestamp
     @Column(name = "created_at", nullable = false)
     val createdAt: LocalDateTime = LocalDateTime.now(),
 
+    @UpdateTimestamp
     @Column(name = "updated_at", nullable = true)
-    val updatedAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime? = null,
 
     @Column(name = "deleted_at", nullable = true)
-    var deletedAt: LocalDateTime = LocalDateTime.now()
+    var deletedAt: LocalDateTime? = null
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
