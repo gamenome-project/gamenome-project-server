8bf578236a485df1ef75e588e9ff582b3c6414fd
임성우 <seongwoorang@naver.com>
2024-05-28 15:49:50 +0900

    Feat: #6 게임 리뷰 crud 도메인 로직 구현

--- src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/review/dto/v1/ReviewDto.kt
+++ src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/review/dto/v1/ReviewDto.kt
@@ -1,11 +1,26 @@
 package sparta.nbcamp.gamenomeprojectserver.domain.review.dto.v1
 
+import sparta.nbcamp.gamenomeprojectserver.domain.review.model.v1.Review
 import java.time.LocalDateTime
 
 data class ReviewDto(
     val gameName: String,
     val title: String,
     val description: String,
+    val isDeleted: Boolean,
     val createdAt: LocalDateTime,
-    val updatedAt: LocalDateTime
-)
+    val updatedAt: LocalDateTime?
+) {
+    companion object {
+        fun from(review: Review): ReviewDto {
+            return ReviewDto(
+                review.gameName,
+                review.title,
+                review.description,
+                review.isDeleted,
+                review.createdAt,
+                review.updatedAt
+            )
+        }
+    }
+}
--- src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/review/service/v1/ReviewServiceImpl.kt
+++ src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/review/service/v1/ReviewServiceImpl.kt
@@ -1,34 +1,44 @@
 package sparta.nbcamp.gamenomeprojectserver.domain.review.service.v1
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import sparta.nbcamp.gamenomeprojectserver.domain.review.dto.v1.ReviewCreateDto
 import sparta.nbcamp.gamenomeprojectserver.domain.review.dto.v1.ReviewDto
 import sparta.nbcamp.gamenomeprojectserver.domain.review.dto.v1.ReviewReportDto
 import sparta.nbcamp.gamenomeprojectserver.domain.review.dto.v1.ReviewUpdateDto
+import sparta.nbcamp.gamenomeprojectserver.domain.review.model.v1.Review
 import sparta.nbcamp.gamenomeprojectserver.domain.review.repository.v1.ReviewRepository
+import sparta.nbcamp.gamenomeprojectserver.exception.ModelNotFoundException
 
 @Service
 class ReviewServiceImpl(
     private val reviewRepository: ReviewRepository
 ) : ReviewService {
+    @Transactional
     override fun createReview(reviewCreateDTO: ReviewCreateDto): ReviewDto {
-        TODO("Not yet implemented")
+        val review = Review(reviewCreateDTO.gameName, reviewCreateDTO.title, reviewCreateDTO.description)
+        return ReviewDto.from(reviewRepository.save(review))
     }
 
     override fun getReviewList(): List<ReviewDto> {
-        TODO("Not yet implemented")
+        val foundAllReview = reviewRepository.findAll()
+        return foundAllReview.map { ReviewDto.from(it) }
     }
-
+    @Transactional
     override fun updateReview(reviewId: Long, reviewUpdateDTO: ReviewUpdateDto): ReviewDto {
-        TODO("Not yet implemented")
+        val foundReview = reviewRepository.findByIdOrNull(reviewId) ?: throw ModelNotFoundException("Review", reviewId)
+        foundReview.updateReviewField(reviewUpdateDTO)
+        return ReviewDto.from(foundReview)
     }
 
     override fun deleteReview(reviewId: Long) {
-        TODO("Not yet implemented")
+        reviewRepository.deleteById(reviewId)
     }
 
     override fun getReview(reviewId: Long): ReviewDto {
-        TODO("Not yet implemented")
+        val foundReview = reviewRepository.findByIdOrNull(reviewId) ?: throw ModelNotFoundException("Review", reviewId)
+        return ReviewDto.from(foundReview)
     }
 
     override fun reportReview(reviewId: Long, reviewReportDTO: ReviewReportDto): ReviewDto {
