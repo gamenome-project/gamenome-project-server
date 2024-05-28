f6d5e7a7937a43208ab31877b4ea83bfa30654d9
Ppajingae <wkmkyj991112@gmail.com>
2024-05-28 11:45:02 +0900

    feat: msg -> description으로 명칭 변경, entityType 추가

--- src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/comment/dto/v1/CommentReportResponseDto.kt
+++ src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/comment/dto/v1/CommentReportResponseDto.kt
@@ -2,5 +2,6 @@ package sparta.nbcamp.gamenomeprojectserver.domain.comment.dto.v1
 
 data class CommentReportResponseDto (
     val userId: Long,
-    val msg: String,
+    val description: String,
+    val entityType: String,
 )
