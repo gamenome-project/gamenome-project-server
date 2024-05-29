6a96f5a0ac98d6ba78987a691de6533d8e8bafec
Jaewon Noh <opqw987@gmail.com>
2024-05-29 12:18:19 +0900

    Feat: Report 유효성 / 불변성 검증 추가

--- src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/comment/dto/v1/ReportCommentRequestDto.kt
+++ src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/domain/comment/dto/v1/ReportCommentRequestDto.kt
@@ -1,8 +1,12 @@
 package sparta.nbcamp.gamenomeprojectserver.domain.comment.dto.v1
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 import org.springframework.context.annotation.Description
 
 data class ReportCommentRequestDto(
+    @field:NotBlank
+    @field:Size(max = 200, message = "Description must be 200 characters or less")
     val description: String,
     val entityType: String = "Comment",
 )
