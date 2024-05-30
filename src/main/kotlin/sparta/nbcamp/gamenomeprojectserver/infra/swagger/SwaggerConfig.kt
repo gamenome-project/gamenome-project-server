bf15f6508464abf7b9e311d7c9f8e67743e374a2
Jaewon Noh <opqw987@gmail.com>
2024-05-30 09:29:23 +0900

    Feat: Swagger에 Authorize 추가

--- src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/infra/swagger/SwaggerConfig.kt
+++ src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/infra/swagger/SwaggerConfig.kt
@@ -1,21 +1,22 @@
 package sparta.nbcamp.gamenomeprojectserver.infra.swagger
 
-import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.Components
-import io.swagger.v3.oas.models.OpenAPI
-import org.springframework.context.annotation.Bean
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class SwaggerConfig {
-
-    @Bean
-    fun openAPI(): OpenAPI = OpenAPI()
-        .components(Components())
-        .info(
-            Info()
-                .title("Gamenome API")
-                .description("Gamenome API schema")
-                .version("1.0.0")
-        )
-}
+@OpenAPIDefinition(
+    info = Info(title = "Gamenome API", description = "Gamenome API schema", version = "1.0.0"),
+    security = [SecurityRequirement(name = "JWT")]
+)
+@SecurityScheme(
+    name = "JWT",
+    type = SecuritySchemeType.APIKEY,
+    `in` = SecuritySchemeIn.HEADER,
+    paramName = "Authorization"
+)
+class SwaggerConfig
