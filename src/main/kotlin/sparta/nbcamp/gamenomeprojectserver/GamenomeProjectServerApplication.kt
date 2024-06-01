b6d34f293f13cf5f24c4d278efaca2c94fd92853
Ppajingae <wkmkyj991112@gmail.com>
2024-06-01 13:48:58 +0900

    refacor : #14 README.md 파일 작성 전 안쓰는 코드 정리

--- src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/GamenomeProjectServerApplication.kt
+++ src/main/kotlin/sparta/nbcamp/gamenomeprojectserver/GamenomeProjectServerApplication.kt
@@ -1,50 +1,13 @@
 package sparta.nbcamp.gamenomeprojectserver
 
-import org.apache.catalina.Context
-import org.apache.catalina.connector.Connector
-import org.apache.tomcat.util.descriptor.web.SecurityCollection
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory
-import org.springframework.context.annotation.Bean
 import sparta.nbcamp.gamenomeprojectserver.domain.security.jwt.JwtProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtProperties::class)
-class GamenomeProjectServerApplication{
-
-    @Bean
-    fun servletContainer(): ServletWebServerFactory {
-
-        val tomcat = object : TomcatServletWebServerFactory() {
-            override fun postProcessContext(context: Context) {
-                val securityConstraint = SecurityConstraint()
-                securityConstraint.userConstraint = "CONFIDENTIAL"
-                val collection = SecurityCollection()
-                collection.addPattern("/*")
-                securityConstraint.addCollection(collection)
-                context.addConstraint(securityConstraint)
-            }
-        }
-
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector())
-        return tomcat
-    }
-
-    fun createStandardConnector(): Connector {
-
-        val connector = Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
-        connector.scheme = "http"
-        connector.port = 8080
-        connector.secure = false
-        connector.redirectPort = 8443
-
-        return connector
-    }
-}
+class GamenomeProjectServerApplication
 
 fun main(args: Array<String>) {
     runApplication<GamenomeProjectServerApplication>(*args)
